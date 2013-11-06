007e60eed90a131c608a843ed07b92b011bcf293
Sebastian <pib.sebastian.schultheiss@htw-saarland.de>
2013-11-06 11:11:46 +0100

    formatierung

--- MADA_ueb01/src/de/mobappdev/ueb01/Ueb01.java
+++ MADA_ueb01/src/de/mobappdev/ueb01/Ueb01.java
@@ -27,7 +27,7 @@ public class Ueb01 extends Activity {
 
 	boolean inflateMenu = false;
 
-	// Klassen fuer grafische Oberfl‰che deklarieren
+	// Klassen fuer grafische Oberflaeche deklarieren
 	private TextView calcField;
 	private TextView txt1;
 	private TextView txt2;
@@ -64,6 +64,7 @@ public class Ueb01 extends Activity {
 	private MenuItem tan;
 	private MenuItem sqrt;
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -75,151 +76,153 @@ public class Ueb01 extends Activity {
 		setMainListeners();
 
 		switch (conf.screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) {
-		case Configuration.SCREENLAYOUT_SIZE_SMALL:
-			inflateAdditionalMenu();
-			break;
-		case Configuration.SCREENLAYOUT_SIZE_NORMAL:
-			inflateAdditionalMenu();
-			break;
-		case Configuration.SCREENLAYOUT_SIZE_LARGE:
-			referenceAdditionalViews();
-			setAdditionalListeners();
-			break;
-		case Configuration.SCREENLAYOUT_SIZE_XLARGE:
-			referenceAdditionalViews();
-			setAdditionalListeners();
-			break;
-		default:
-			inflateAdditionalMenu();
-			break;
+			case Configuration.SCREENLAYOUT_SIZE_SMALL:
+				inflateAdditionalMenu();
+				break;
+			case Configuration.SCREENLAYOUT_SIZE_NORMAL:
+				inflateAdditionalMenu();
+				break;
+			case Configuration.SCREENLAYOUT_SIZE_LARGE:
+				referenceAdditionalViews();
+				setAdditionalListeners();
+				break;
+			case Configuration.SCREENLAYOUT_SIZE_XLARGE:
+				referenceAdditionalViews();
+				setAdditionalListeners();
+				break;
+			default:
+				inflateAdditionalMenu();
+				break;
 		}
 	}
 
+	
 	private OnClickListener generalButtonListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
 			switch (v.getId()) {
-			case R.id.ueb01_1:
-				addToCalcField(getString(R.string.ueb01_1));
-				break;
-			case R.id.ueb01_2:
-				addToCalcField(getString(R.string.ueb01_2));
-				break;
-			case R.id.ueb01_3:
-				addToCalcField(getString(R.string.ueb01_3));
-				break;
-			case R.id.ueb01_4:
-				addToCalcField(getString(R.string.ueb01_4));
-				break;
-			case R.id.ueb01_5:
-				addToCalcField(getString(R.string.ueb01_5));
-				break;
-			case R.id.ueb01_6:
-				addToCalcField(getString(R.string.ueb01_6));
-				break;
-			case R.id.ueb01_7:
-				addToCalcField(getString(R.string.ueb01_7));
-				break;
-			case R.id.ueb01_8:
-				addToCalcField(getString(R.string.ueb01_8));
-				break;
-			case R.id.ueb01_9:
-				addToCalcField(getString(R.string.ueb01_9));
-				break;
-			case R.id.ueb01_0:
-				addToCalcField(getString(R.string.ueb01_0));
-				break;
-			case R.id.ueb01_minus:
-				addToCalcField(getString(R.string.ueb01_minus));
-				break;
-			case R.id.ueb01_plus:
-				addToCalcField(getString(R.string.ueb01_plus));
-				break;
-			case R.id.ueb01_mul:
-				addToCalcField(getString(R.string.ueb01_mul));
-				break;
-			case R.id.ueb01_divide:
-				addToCalcField(getString(R.string.ueb01_divide));
-				break;
-			case R.id.ueb01_lpar:
-				addToCalcField(getString(R.string.ueb01_lpar));
-				break;
-			case R.id.ueb01_rpar:
-				addToCalcField(getString(R.string.ueb01_rpar));
-				break;
-			case R.id.ueb01_mod:
-				addToCalcField(getString(R.string.ueb01_mod));
-				break;
-			case R.id.ueb01_pow:
-				addToCalcField(getString(R.string.ueb01_pow));
-				break;
-			default:
-				// Do nothing
-				break;
-
+				case R.id.ueb01_1:
+					addToCalcField(getString(R.string.ueb01_1));
+					break;
+				case R.id.ueb01_2:
+					addToCalcField(getString(R.string.ueb01_2));
+					break;
+				case R.id.ueb01_3:
+					addToCalcField(getString(R.string.ueb01_3));
+					break;
+				case R.id.ueb01_4:
+					addToCalcField(getString(R.string.ueb01_4));
+					break;
+				case R.id.ueb01_5:
+					addToCalcField(getString(R.string.ueb01_5));
+					break;
+				case R.id.ueb01_6:
+					addToCalcField(getString(R.string.ueb01_6));
+					break;
+				case R.id.ueb01_7:
+					addToCalcField(getString(R.string.ueb01_7));
+					break;
+				case R.id.ueb01_8:
+					addToCalcField(getString(R.string.ueb01_8));
+					break;
+				case R.id.ueb01_9:
+					addToCalcField(getString(R.string.ueb01_9));
+					break;
+				case R.id.ueb01_0:
+					addToCalcField(getString(R.string.ueb01_0));
+					break;
+				case R.id.ueb01_minus:
+					addToCalcField(getString(R.string.ueb01_minus));
+					break;
+				case R.id.ueb01_plus:
+					addToCalcField(getString(R.string.ueb01_plus));
+					break;
+				case R.id.ueb01_mul:
+					addToCalcField(getString(R.string.ueb01_mul));
+					break;
+				case R.id.ueb01_divide:
+					addToCalcField(getString(R.string.ueb01_divide));
+					break;
+				case R.id.ueb01_lpar:
+					addToCalcField(getString(R.string.ueb01_lpar));
+					break;
+				case R.id.ueb01_rpar:
+					addToCalcField(getString(R.string.ueb01_rpar));
+					break;
+				case R.id.ueb01_mod:
+					addToCalcField(getString(R.string.ueb01_mod));
+					break;
+				case R.id.ueb01_pow:
+					addToCalcField(getString(R.string.ueb01_pow));
+					break;
+				default:
+					// Do nothing
+					break;
 			}
 
 		}
 	};
 
+	
 	private OnClickListener extendedButtonListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
 			switch (v.getId()) {
-			case R.id.ueb01_sin:
-				addToCalcField(getString(R.string.ueb01_sin));
-				break;
-			case R.id.ueb01_cos:
-				addToCalcField(getString(R.string.ueb01_cos));
-				break;
-			case R.id.ueb01_tan:
-				addToCalcField(getString(R.string.ueb01_tan));
-				break;
-			case R.id.ueb01_sqrt:
-				addToCalcField(getString(R.string.ueb01_sqrt));
-				break;
-			default:
-				// Do nothing
-				break;
+				case R.id.ueb01_sin:
+					addToCalcField(getString(R.string.ueb01_sin));
+					break;
+				case R.id.ueb01_cos:
+					addToCalcField(getString(R.string.ueb01_cos));
+					break;
+				case R.id.ueb01_tan:
+					addToCalcField(getString(R.string.ueb01_tan));
+					break;
+				case R.id.ueb01_sqrt:
+					addToCalcField(getString(R.string.ueb01_sqrt));
+					break;
+				default:
+					// Do nothing
+					break;
 			}
 			addToCalcField(getString(R.string.ueb01_lpar));
 		}
 	};
 
+	
 	private OnLongClickListener extendedButtonLongListener = new OnLongClickListener() {
 		@Override
 		public boolean onLongClick(View v) {
 			switch (v.getId()) {
-			case R.id.ueb01_sin:
-				addToCalcFieldFront(getString(R.string.ueb01_lpar));
-				addToCalcFieldFront(getString(R.string.ueb01_sin));
-				addToCalcField(getString(R.string.ueb01_rpar));
-				return true;
-			case R.id.ueb01_cos:
-				addToCalcFieldFront(getString(R.string.ueb01_lpar));
-				addToCalcFieldFront(getString(R.string.ueb01_cos));
-				addToCalcField(getString(R.string.ueb01_rpar));
-				return true;
-			case R.id.ueb01_tan:
-				addToCalcFieldFront(getString(R.string.ueb01_lpar));
-				addToCalcFieldFront(getString(R.string.ueb01_tan));
-				addToCalcField(getString(R.string.ueb01_rpar));
-				return true;
-			case R.id.ueb01_sqrt:
-				addToCalcFieldFront(getString(R.string.ueb01_lpar));
-				addToCalcFieldFront(getString(R.string.ueb01_sqrt));
-				addToCalcField(getString(R.string.ueb01_rpar));
-				return true;
-			default:
-				// Do nothing
-				return true;
+				case R.id.ueb01_sin:
+					addToCalcFieldFront(getString(R.string.ueb01_lpar));
+					addToCalcFieldFront(getString(R.string.ueb01_sin));
+					addToCalcField(getString(R.string.ueb01_rpar));
+					return true;
+				case R.id.ueb01_cos:
+					addToCalcFieldFront(getString(R.string.ueb01_lpar));
+					addToCalcFieldFront(getString(R.string.ueb01_cos));
+					addToCalcField(getString(R.string.ueb01_rpar));
+					return true;
+				case R.id.ueb01_tan:
+					addToCalcFieldFront(getString(R.string.ueb01_lpar));
+					addToCalcFieldFront(getString(R.string.ueb01_tan));
+					addToCalcField(getString(R.string.ueb01_rpar));
+					return true;
+				case R.id.ueb01_sqrt:
+					addToCalcFieldFront(getString(R.string.ueb01_lpar));
+					addToCalcFieldFront(getString(R.string.ueb01_sqrt));
+					addToCalcField(getString(R.string.ueb01_rpar));
+					return true;
+				default:
+					// Do nothing
+					return true;
 			}
 		}
 	};
 
+	
 	/**
-	 * Referenziert die Java-Variablen mit dem entsprechendem Objekt in der
-	 * XML-Datei
+	 * Referenziert die Java-Variablen mit dem entsprechendem Objekt in der XML-Datei
 	 */
 	private void referenceMainViews() {
 		calcField = (TextView) findViewById(R.id.ueb01_calcTextField);
@@ -246,6 +249,9 @@ public class Ueb01 extends Activity {
 		backImg = (ImageView) findViewById(R.id.ueb01_backBT);
 	}
 
+	/**
+	 * Referenziert zusaetzliche Listener fuer Buttons auf Tablets
+	 */
 	private void referenceAdditionalViews() {
 		txtSin = (TextView) findViewById(R.id.ueb01_sin);
 		txtCos = (TextView) findViewById(R.id.ueb01_cos);
@@ -253,15 +259,18 @@ public class Ueb01 extends Activity {
 		txtSqrt = (TextView) findViewById(R.id.ueb01_sqrt);
 	}
 
+	/**
+	 * Bestimmt, ob das Optionsmenue verfuegbar wird
+	 */
 	private void inflateAdditionalMenu() {
 		inflateMenu = true;
 	}
 
+	
 	/**
 	 * Setzt die Listener, die immer da sind
 	 */
 	private void setMainListeners() {
-
 		txt1.setOnClickListener(generalButtonListener);
 		txt2.setOnClickListener(generalButtonListener);
 		txt3.setOnClickListener(generalButtonListener);
@@ -286,8 +295,7 @@ public class Ueb01 extends Activity {
 			@Override
 			public void onClick(View v) {
 				if (calcField.length() < 1) {
-					addToCalcField(getString(R.string.ueb01_0)
-							+ getString(R.string.ueb01_komma));
+					addToCalcField(getString(R.string.ueb01_0) + getString(R.string.ueb01_komma));
 				} else {
 					addToCalcField(getString(R.string.ueb01_komma));
 				}
@@ -298,8 +306,7 @@ public class Ueb01 extends Activity {
 			@Override
 			public void onClick(View v) {
 				if (calcField.length() > 0) {
-					calcField.setText(calcField.getText().subSequence(0,
-							calcField.length() - 1));
+					calcField.setText(calcField.getText().subSequence(0, calcField.length()-1));
 				}
 			}
 		});
@@ -318,20 +325,14 @@ public class Ueb01 extends Activity {
 			public void onClick(View v) {
 				try {
 					// Uebergibt den mathematischen Ausdruck der Calc-Library
-					calc = new ExpressionBuilder(calcField
-							.getText()
-							.toString()
-							.replace(getString(R.string.ueb01_mul),
-									getString(R.string.ueb01_mul_calc)))
-							.build();
+					calc = new ExpressionBuilder(calcField.getText().toString().replace(getString(R.string.ueb01_mul),
+							getString(R.string.ueb01_mul_calc))).build();
 					Double result = calc.calculate();
 					// Schreibt das Ergebnis in das Ergebnis-Textfeld
 					calcField.setText(result.toString());
 				} catch (Exception e) {
 					// Abfangen eventueller Exceptions
-					Toast.makeText(getApplicationContext(),
-							getString(R.string.ueb01_wrongInput),
-							Toast.LENGTH_SHORT).show();
+					Toast.makeText(getApplicationContext(), getString(R.string.ueb01_wrongInput), Toast.LENGTH_SHORT).show();
 					e.printStackTrace();
 				}
 			}
@@ -339,6 +340,7 @@ public class Ueb01 extends Activity {
 
 	}
 
+	
 	/**
 	 * Setzt zusaetzliche Listener fuer Geraete mit groﬂen Displays
 	 */
@@ -353,18 +355,21 @@ public class Ueb01 extends Activity {
 		txtSqrt.setOnLongClickListener(extendedButtonLongListener);
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.math_menu, menu);
 		return inflateMenu;
 	}
 
+	/**
+	 * Aktualisiert die Anzeige der Menueeintraege
+	 */
 	private void setFunktionTitles() {
-
 		int subLimiter = calcField.length();
 		String extension = "";
 
-		if (calcField.length() == 0) {
+		if (calcField.length() < 1) {
 			extension = getString(R.string.ueb01_three_dots);
 		} else {
 			if (subLimiter > 4) {
@@ -372,24 +377,30 @@ public class Ueb01 extends Activity {
 				extension = getString(R.string.ueb01_three_dots);
 			}
 		}
+		
 		sin.setTitle(getString(R.string.ueb01_menu_item_sin_front)
 				+ getString(R.string.ueb01_lpar)
-				+ calcField.getText().subSequence(0, subLimiter) + extension
+				+ calcField.getText().subSequence(0, subLimiter)
+				+ extension
 				+ getString(R.string.ueb01_rpar));
 		cos.setTitle(getString(R.string.ueb01_menu_item_cos_front)
 				+ getString(R.string.ueb01_lpar)
-				+ calcField.getText().subSequence(0, subLimiter) + extension
+				+ calcField.getText().subSequence(0, subLimiter)
+				+ extension
 				+ getString(R.string.ueb01_rpar));
 		tan.setTitle(getString(R.string.ueb01_menu_item_tan_front)
 				+ getString(R.string.ueb01_lpar)
-				+ calcField.getText().subSequence(0, subLimiter) + extension
+				+ calcField.getText().subSequence(0, subLimiter)
+				+ extension
 				+ getString(R.string.ueb01_rpar));
 		sqrt.setTitle(getString(R.string.ueb01_menu_item_sqrt_front)
 				+ getString(R.string.ueb01_lpar)
-				+ calcField.getText().subSequence(0, subLimiter) + extension
+				+ calcField.getText().subSequence(0, subLimiter)
+				+ extension
 				+ getString(R.string.ueb01_rpar));
 	}
 
+	
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
 		sin = menu.findItem(R.id.menu_item_sin_front);
@@ -400,64 +411,67 @@ public class Ueb01 extends Activity {
 		return super.onPrepareOptionsMenu(menu);
 	}
 
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case R.id.menu_item_more:
-			setFunktionTitles();
-			break;
-		case R.id.menu_item_sin_front:
-			addToCalcFieldFront(getString(R.string.ueb01_lpar));
-			addToCalcFieldFront(getString(R.string.ueb01_sin));
-			addToCalcField(getString(R.string.ueb01_rpar));
-			break;
-		case R.id.menu_item_cos_front:
-			addToCalcFieldFront(getString(R.string.ueb01_lpar));
-			addToCalcFieldFront(getString(R.string.ueb01_cos));
-			addToCalcField(getString(R.string.ueb01_rpar));
-			break;
-		case R.id.menu_item_tan_front:
-			addToCalcFieldFront(getString(R.string.ueb01_lpar));
-			addToCalcFieldFront(getString(R.string.ueb01_tan));
-			addToCalcField(getString(R.string.ueb01_rpar));
-			break;
-		case R.id.menu_item_sqrt_front:
-			addToCalcFieldFront(getString(R.string.ueb01_lpar));
-			addToCalcFieldFront(getString(R.string.ueb01_sqrt));
-			addToCalcField(getString(R.string.ueb01_rpar));
-			break;
-		case R.id.menu_item_sin_back:
-			addToCalcField(getString(R.string.ueb01_sin));
-			addToCalcField(getString(R.string.ueb01_lpar));
-			break;
-		case R.id.menu_item_cos_back:
-			addToCalcField(getString(R.string.ueb01_cos));
-			addToCalcField(getString(R.string.ueb01_lpar));
-			break;
-		case R.id.menu_item_tan_back:
-			addToCalcField(getString(R.string.ueb01_tan));
-			addToCalcField(getString(R.string.ueb01_lpar));
-			break;
-		case R.id.menu_item_sqrt_back:
-			addToCalcField(getString(R.string.ueb01_sqrt));
-			addToCalcField(getString(R.string.ueb01_lpar));
-			break;
-		default:
-			// do nothing
-			return true;
+			case R.id.menu_item_more:
+				setFunktionTitles();
+				break;
+			case R.id.menu_item_sin_front:
+				addToCalcFieldFront(getString(R.string.ueb01_lpar));
+				addToCalcFieldFront(getString(R.string.ueb01_sin));
+				addToCalcField(getString(R.string.ueb01_rpar));
+				break;
+			case R.id.menu_item_cos_front:
+				addToCalcFieldFront(getString(R.string.ueb01_lpar));
+				addToCalcFieldFront(getString(R.string.ueb01_cos));
+				addToCalcField(getString(R.string.ueb01_rpar));
+				break;
+			case R.id.menu_item_tan_front:
+				addToCalcFieldFront(getString(R.string.ueb01_lpar));
+				addToCalcFieldFront(getString(R.string.ueb01_tan));
+				addToCalcField(getString(R.string.ueb01_rpar));
+				break;
+			case R.id.menu_item_sqrt_front:
+				addToCalcFieldFront(getString(R.string.ueb01_lpar));
+				addToCalcFieldFront(getString(R.string.ueb01_sqrt));
+				addToCalcField(getString(R.string.ueb01_rpar));
+				break;
+			case R.id.menu_item_sin_back:
+				addToCalcField(getString(R.string.ueb01_sin));
+				addToCalcField(getString(R.string.ueb01_lpar));
+				break;
+			case R.id.menu_item_cos_back:
+				addToCalcField(getString(R.string.ueb01_cos));
+				addToCalcField(getString(R.string.ueb01_lpar));
+				break;
+			case R.id.menu_item_tan_back:
+				addToCalcField(getString(R.string.ueb01_tan));
+				addToCalcField(getString(R.string.ueb01_lpar));
+				break;
+			case R.id.menu_item_sqrt_back:
+				addToCalcField(getString(R.string.ueb01_sqrt));
+				addToCalcField(getString(R.string.ueb01_lpar));
+				break;
+			default:
+				// do nothing
+				return true;
 		}
 		return true;
 	}
 
+	
 	/**
-	 * H‰ngt den Wert der gedrueckten Taste an das Ergebnisfeld an.
+	 * Haengt den Wert der gedrueckten Taste hinten an das Ergebnisfeld an.
 	 */
 	private void addToCalcField(String txt) {
 		calcField.setText(calcField.getText() + txt);
 	}
 
+	
 	/**
-	 * H‰ngt den Wert der gedrueckten Taste an das Ergebnisfeld an.
+	 * Haengt den Wert der gedrueckten Taste vorne an das Ergebnisfeld an.
 	 */
 	private void addToCalcFieldFront(String txt) {
 		calcField.setText(txt + calcField.getText());
