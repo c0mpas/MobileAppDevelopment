2017f2f2dd92d45844bf66e2868157543b29d8b0
Sebastian <pib.sebastian.schultheiss@htw-saarland.de>
2013-11-11 13:10:50 +0100

    added update of pref summarys

--- MADA_ueb03/src/com/example/mada_ueb03/SettingsActivity.java
+++ MADA_ueb03/src/com/example/mada_ueb03/SettingsActivity.java
@@ -1,13 +1,17 @@
 package com.example.mada_ueb03;
 
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.widget.EditText;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	
 	public static final String DEFAULT_HEAD_SIZE = "14";
 	public static final String DEFAULT_TAIL_SIZE = "10";
@@ -30,21 +34,51 @@ public class SettingsActivity extends PreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
-		updatePreferences();
+		referencePreferences();
+		updatePreferenceSummarys();
 	}
 
 	@SuppressWarnings("deprecation")
-	private void updatePreferences() {
+	private void updatePreferenceSummarys() {
 		prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		
-		prefFontsizeHead = (EditTextPreference) findPreference(FONTSIZE_HEAD);
-		prefFontsizeTail = (EditTextPreference) findPreference(FONTSIZE_TAIL);
-		prefTheme = (ListPreference) findPreference(THEME);
 		
 		prefFontsizeHead.setSummary(prefs.getString(FONTSIZE_HEAD, DEFAULT_HEAD_SIZE));
 		prefFontsizeTail.setSummary(prefs.getString(FONTSIZE_TAIL, DEFAULT_TAIL_SIZE));
 		prefTheme.setSummary(prefTheme.getEntry());
-		
+	}
+    
+	@SuppressWarnings("deprecation")
+	private void referencePreferences() {
+		prefFontsizeHead = (EditTextPreference) findPreference(FONTSIZE_HEAD);
+		prefFontsizeTail = (EditTextPreference) findPreference(FONTSIZE_TAIL);
+		prefTheme = (ListPreference) findPreference(THEME);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		if (key.equals(FONTSIZE_HEAD)) {
+			EditTextPreference pref = (EditTextPreference) findPreference(FONTSIZE_HEAD);
+			pref.setSummary(((EditText) pref.getEditText()).getText());
+		} else if (key.equals(FONTSIZE_TAIL)) {
+			EditTextPreference pref = (EditTextPreference) findPreference(FONTSIZE_TAIL);
+			pref.setSummary(((EditText) pref.getEditText()).getText());
+		} else if (key.equals(THEME)) {
+			ListPreference pref = (ListPreference) findPreference(THEME);
+			pref.setSummary(pref.getEntry());
+		}
 	}
 	
+	@Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+    
 }
