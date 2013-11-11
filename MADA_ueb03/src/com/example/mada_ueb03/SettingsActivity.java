3d362dce3fd5bd2b2b6c7d724f56bfbfd8b4ac11
Christoph <Christoph@chris-lap.hiz-saarland.de>
2013-11-11 14:59:00 +0100

    Linited font size

--- MADA_ueb03/src/com/example/mada_ueb03/SettingsActivity.java
+++ MADA_ueb03/src/com/example/mada_ueb03/SettingsActivity.java
@@ -9,10 +9,13 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.EditText;
 
-public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
-	
+public class SettingsActivity extends PreferenceActivity implements
+		OnSharedPreferenceChangeListener {
+
 	public static final String DEFAULT_HEAD_SIZE = "20";
 	public static final String DEFAULT_TAIL_SIZE = "16";
 	public static final String DEFAULT_THEME = "0";
@@ -20,23 +23,22 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 	public static final String FONTSIZE_TAIL = "fontsize_tail";
 	public static final String THEME = "theme";
 
-	public static final String	THEME_ANDROID = "Android";
-	public static final int		THEME_VALUE_ANDROID = 0;
-	public static final String	THEME_KITTY = "Kitty";
-	public static final int		THEME_VALUE_KITTY = 1;
-	public static final String	THEME_DARK = "Dark";
-	public static final int		THEME_VALUE_DARK = 2;
-	private String[] themes = {THEME_ANDROID, THEME_KITTY, THEME_DARK};
-	
+	public static final String THEME_ANDROID = "Android";
+	public static final int THEME_VALUE_ANDROID = 0;
+	public static final String THEME_KITTY = "Kitty";
+	public static final int THEME_VALUE_KITTY = 1;
+	public static final String THEME_DARK = "Dark";
+	public static final int THEME_VALUE_DARK = 2;
+	private String[] themes = { THEME_ANDROID, THEME_KITTY, THEME_DARK };
+
 	SharedPreferences prefs;
-	
+
 	int head, tail, theme;
-	
+
 	EditTextPreference prefFontsizeHead;
 	EditTextPreference prefFontsizeTail;
 	ListPreference prefTheme;
 
-	
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,22 +51,49 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 	@SuppressWarnings("deprecation")
 	private void updatePreferenceSummarys() {
 		prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		prefFontsizeHead.setSummary(prefs.getString(FONTSIZE_HEAD, DEFAULT_HEAD_SIZE));
-		prefFontsizeTail.setSummary(prefs.getString(FONTSIZE_TAIL, DEFAULT_TAIL_SIZE));
+		prefFontsizeHead.setSummary(prefs.getString(FONTSIZE_HEAD,
+				DEFAULT_HEAD_SIZE));
+		prefFontsizeTail.setSummary(prefs.getString(FONTSIZE_TAIL,
+				DEFAULT_TAIL_SIZE));
 		prefTheme.setSummary(prefTheme.getEntry());
 	}
-    
+
 	@SuppressWarnings("deprecation")
 	private void initializePreferences() {
 		prefFontsizeHead = (EditTextPreference) findPreference(FONTSIZE_HEAD);
+		prefFontsizeHead.getEditText().setOnKeyListener(
+				new EditText.OnKeyListener() {
+
+					@Override
+					public boolean onKey(View v, int keyCode, KeyEvent event) {
+						if (prefFontsizeHead.getEditText().getText().length() > 1)
+							return true;
+
+						return false;
+					}
+				});
+
 		prefFontsizeTail = (EditTextPreference) findPreference(FONTSIZE_TAIL);
+		prefFontsizeTail.getEditText().setOnKeyListener(
+				new EditText.OnKeyListener() {
+
+					@Override
+					public boolean onKey(View v, int keyCode, KeyEvent event) {
+						if (prefFontsizeTail.getEditText().getText().length() > 1)
+							return true;
+
+						return false;
+					}
+				});
+		
 		prefTheme = (ListPreference) findPreference(THEME);
 		prefTheme.setEntries(themes);
 	}
 
 	@SuppressWarnings("deprecation")
 	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
 		if (key.equals(FONTSIZE_HEAD)) {
 			EditTextPreference pref = (EditTextPreference) findPreference(FONTSIZE_HEAD);
 			pref.setSummary(((EditText) pref.getEditText()).getText());
@@ -77,17 +106,19 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 		}
 		setResult(RESULT_OK);
 	}
-	
-	@Override
-    protected void onResume() {
-        super.onResume();
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
-    }
-    
+	@Override
+	protected void onResume() {
+		super.onResume();
+		getPreferenceScreen().getSharedPreferences()
+				.registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		getPreferenceScreen().getSharedPreferences()
+				.unregisterOnSharedPreferenceChangeListener(this);
+	}
+
 }
