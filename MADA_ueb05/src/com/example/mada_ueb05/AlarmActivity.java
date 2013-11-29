e42704bbb9dca7b9fefcdd82267c35e9a7c0f879
Sebastian <pib.sebastian.schultheiss@htw-saarland.de>
2013-11-29 13:17:58 +0100

    added alarm

--- MADA_ueb05/src/com/example/mada_ueb05/AlarmActivity.java
+++ MADA_ueb05/src/com/example/mada_ueb05/AlarmActivity.java
@@ -1,26 +1,70 @@
 package com.example.mada_ueb05;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class AlarmActivity extends Activity {
 
+	private static final int ALARMTIME = 30000;
+	
 	private Button off;
 	private Button snooze;
+	private MediaPlayer player;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_alarm);
+		referenceViews();
+		setListeners();
+		startAlarm();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.alarm, menu);
-		return true;
+	private void referenceViews() {
+		off = (Button) findViewById(R.id.button_off);
+		snooze = (Button) findViewById(R.id.button_snooze);
+	}
+	
+	private void setListeners() {
+		off.setOnClickListener(offListener);
+		snooze.setOnClickListener(snoozeListener);
+	}
+	
+	private void startAlarm() {
+		player = MediaPlayer.create(this, R.raw.alarm);
+		player.setLooping(true);
+		player.start();
+		SystemClock.sleep(ALARMTIME);
+		player.stop();
 	}
 
+	private void stopAlarm() {
+		// turn off alarm sound
+	}
+
+	private void snoozeAlarm() {
+		// snooze alarm sound
+	}
+	
+	private OnClickListener offListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			stopAlarm();
+		}
+	};
+
+	private OnClickListener snoozeListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			snoozeAlarm();
+		}
+	};
+	
 }
