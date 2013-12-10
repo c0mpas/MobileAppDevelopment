d376305c6922b51807b17dc4a0301b01365730a6
Christoph <Christoph@chris-lap.hiz-saarland.de>
2013-12-10 18:47:30 +0100

    ss

--- TaskList/src/com/example/tasklist/ToDoDBHelper.java
+++ TaskList/src/com/example/tasklist/ToDoDBHelper.java
@@ -51,12 +51,6 @@ public class ToDoDBHelper {
 		dao.delete(task);
 	}
 
-	public List<ToDoTask> where(Context context, String column, Object value)
-			throws SQLException {
-		Dao<ToDoTask, Integer> dao = getHelper(context).createTodoDAO();
-		List<ToDoTask> tasks = dao.queryBuilder().where()
-				.eq(column, value).query();
-		return tasks;
-	}
+
 
 }
